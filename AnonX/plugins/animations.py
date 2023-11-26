121b1274aef73da442aaf6e3f17bdc1d0d4edbc8
Noobxcoders <128674386+Noobxcoders@users.noreply.github.com>
2023-11-26 22:51:24 +0530

    Update animations.py

--- AnonX/plugins/animations.py
+++ AnonX/plugins/animations.py
@@ -5,7 +5,7 @@ import os
 import re
 import asyncio
 import time
-from AarohiX import app 
+from AnonX import app 
 
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton 
 
