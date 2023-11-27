924462f9a175d57f9e85a4306605ee74ea6ea946
Noobxcoders <128674386+Noobxcoders@users.noreply.github.com>
2023-11-27 08:25:21 +0530

    Update Theme.py

--- AnonX/plugins/Theme.py
+++ AnonX/plugins/Theme.py
@@ -2,9 +2,9 @@ from typing import Dict, List, Union
 
 from pyrogram import Client, filters
 
-from Yukki import BOT_USERNAME, MUSIC_BOT_NAME, app, db
-from Yukki.Database import _get_theme, get_theme, save_theme
-from Yukki.Decorators.permission import PermissionCheck
+from AnonX import BOT_USERNAME, MUSIC_BOT_NAME, app, db
+from AnonX.Database import _get_theme, get_theme, save_theme
+from AnonX.Decorators.permission import PermissionCheck
 
 themes = [
     "blue",
