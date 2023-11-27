28690b08b82eca3e2cf51f5f6000c028c00c686a
Noobxcoders <128674386+Noobxcoders@users.noreply.github.com>
2023-11-27 08:35:14 +0530

    Update Theme.py

--- AnonX/plugins/Theme.py
+++ AnonX/plugins/Theme.py
@@ -2,7 +2,7 @@ from typing import Dict, List, Union
 
 from pyrogram import Client, filters
 
-from AnonX import BOT_USERNAME, MUSIC_BOT_NAME, app, db
+from AnonX import app, db
 from AnonX.Database import _get_theme, get_theme, save_theme
 from AnonX.Decorators.permission import PermissionCheck
 
